28ca6e03cc15b799f3176010f319d8afee54af74
Invisgoth <hectorjdgdevelop@gmail.com>
2024-01-23 15:12:12 +0100

    chore: Cosmetic changes

--- app/src/main/java/com/dsoles/mobile/getyourcats/modules/home/network/BreedRespositoryImp.kt
+++ app/src/main/java/com/dsoles/mobile/getyourcats/modules/home/network/BreedRespositoryImp.kt
@@ -1,13 +1,12 @@
 package com.dsoles.mobile.getyourcats.modules.home.network
 
 import com.dsoles.mobile.getyourcats.modules.home.data.Breed
-import com.dsoles.mobile.getyourcats.utils.ConfigApi
 import com.dsoles.mobile.getyourcats.utils.RequestState
 import javax.inject.Inject
 
 class BreedRespositoryImp @Inject constructor(private val breedService: BreedService) :
     BreedRepository {
-    override suspend fun getBreeds(page: Int,pageSize: Int): RequestState<List<Breed>> {
+    override suspend fun getBreeds(page: Int, pageSize: Int): RequestState<List<Breed>> {
         return try {
             RequestState.Success(breedService.getBreeds(page = page, limit = pageSize))
         } catch (e: Exception) {
